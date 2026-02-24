f0e45427e09db4ee2cfc136fe760e2d25e6fcdb0
Sreeharivs1983 <sreeharivs69@gmail.com>
2026-02-23 23:43:34 -0500

    Instant Consultation Added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:BIKCwRUMGsFTCzVwr3PxhidC@172.21.245.104:27017";
+const mongoURI =  "mongodb://root:FSwHMVVRJBV8WJ3EMZiVypbl@172.21.2.244:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
